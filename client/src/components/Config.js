c07a01bc68b5cae48e9a86c7c5765e45534b4130
nan-noo <uni613@naver.com>
2021-12-01 16:00:37 +0900

    feat: update Config.js
    Deleted BASE_SERVER

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,4 +1,3 @@
-export const BASE_SERVER = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
 export const USER_SERVER = '/api/users';
 export const POST_SERVER = '/api/posts';
 export const COMMENT_SERVER = '/api/comments';
