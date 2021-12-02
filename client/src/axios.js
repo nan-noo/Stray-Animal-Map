5a7b251933ea76f4c179a447bc7b19eb212a543d
nan-noo <uni613@naver.com>
2021-12-02 17:37:40 +0900

    feat: add external server url

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { SERVER_URL } from './secret';
 
-axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? '/' : '';
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? '/' : SERVER_URL;
 export default axios;
